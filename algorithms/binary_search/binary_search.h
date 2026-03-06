818ba1704eb8e2134ed17be2edffa6453ff43d64
Ivan Leshchenko <leshchenkoivan@gmail.com>
2026-03-06 11:35:30 +0200

    binary search added

---
+++ algorithms/binary_search/binary_search.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <vector>
+
+int binarySearch(const std::vector<int>& arr, int target);
---
+++ algorithms/tests/binary_search/binary_search_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#include "../../binary_search/binary_search.h"
+
+int main() {
+    std::vector<int> arr{1,2,3,4,5,6,7,8,9,10};
+
+    assert(binarySearch(arr, 1) == 0);
+    assert(binarySearch(arr, 5) == 4);
+    assert(binarySearch(arr, 10) == 9);
+    assert(binarySearch(arr, 11) == -1);
+
+    std::cout << "All tests passed\n";
+}
