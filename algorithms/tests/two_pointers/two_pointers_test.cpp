9a132be98916cdac3599f120c30a416f3631460f
Ivan Leshchenko <ivan.leshchenko@rxdefine.com>
2026-03-09 21:15:36 +0200

    two pointers added

---
+++ algorithms/tests/two_pointers/two_pointers_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <utility>
+
+#include "../../two_pointers/two_pointers.h"
+
+int main() {
+    std::vector<int> arr{1,2,3,4,5,6,7,8,9,10};
+
+    assert((two_pointers(arr, 12) == std::pair<int, int>{1, 9}));
+    assert((two_pointers(arr, 20) == std::pair<int, int>{-1, -1}));
+    assert((two_pointers(arr, 3) == std::pair<int, int>{0, 1}));
+
+    std::cout << "All tests passed\n";
+}
---
+++ algorithms/two_pointers/two_pointers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+std::pair<int, int> two_pointers(const std::vector<int>& arr, int target);
